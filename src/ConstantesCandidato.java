8d716bd99fa399a7cf6f3f8fd6ccbed7c6e6cd50
Pedro2um1dois <pedroigorgm@gmail.com>
2022-11-15 21:53:04 -0300

    Delete ConstantesCandidato.java

--- src/ConstantesCandidato.java
+++
@@ -1,7 +0,0 @@
-public final class ConstantesCandidato {
-        static final Integer DEPUTADO_FEDERAL = 6;
-        static final Integer DEPUTADO_ESTADUAL = 7;
-        static final Integer[] DEFERIDO = {2, 16};
-        static final Integer MASCULINO = 2;
-        static final Integer FEMINIMO = 4;
-}
