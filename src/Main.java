752034d3226dad705f7207d44ae4469010dd8f6d
Pedro2um1dois <pedroigorgm@gmail.com>
2022-12-04 16:29:19 -0300

    nice

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ import java.util.Map;
 
 public class Main{
         public static void main(String[] args) throws Exception{  
-                String enconding = "ISO-8859-1";
-                Map<Integer,Partido> part = new HashMap<>(); 
-                Map<Integer,Candidato> cand = new HashMap<>(); 
-                Locale locale = new Locale("pt", "BR");
-                Eleicao eleicao = new Eleicao(locale, args[3]);
-                eleicao.readInput(enconding, part, cand,  args[0], args[1], args[2], args[3]);
-                eleicao.solve(part, cand, args[3]);
-                eleicao.writeOutput(part, args[0], args[3]);
+                try{
+                        String enconding = "ISO-8859-1";
+                        Map<Integer,Partido> part = new HashMap<>(); 
+                        Map<Integer,Candidato> cand = new HashMap<>(); 
+                        Locale locale = new Locale("pt", "BR");
+                        Eleicao eleicao = new Eleicao(locale, args[3]);
+                        eleicao.readInput(enconding, part, cand,  args[0], args[1], args[2], args[3]);
+                        eleicao.solve(part, cand, args[3]);
+                        eleicao.writeOutput(part, args[0], args[3]);
+                } catch(ArrayIndexOutOfBoundsException e){
+                        System.out.println("Erro, formato dos parametro eh: '--cargo' 'candidato.csv' 'votacao.csv' 'data' " );
+                }
         }          
 }
